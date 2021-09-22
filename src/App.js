e4e4e49302165c1676c7a088f4cef76bb2ebeb44
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-22 20:45:25 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import useWindowSize from "./hooks/useWindowSize";
-import Login from "./components/Login"
+import Login from "./components/Login";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 
 export default function App() {
   const page = useWindowSize();
-  return <Login />;
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return <Login />;
+  }
+
+  console.log({ user });
 
   return (
     <div className="app" style={{ ...page }}>
