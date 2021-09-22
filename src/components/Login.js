546d4fcae69ac5a864825f8ec051ebdd3ad385ee
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-22 12:25:23 +0530

    login added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,24 @@
 import "./Login.css";
+import { auth, provider } from "../firebase";
+import { Button } from "@material-ui/core";
 
 export default function Login() {
-  return <div className="app">Login</div>;
+  function login() {
+    auth.signInWithRedirect(provider);
+  }
+  return (
+    <div className="app">
+      <div className="login">
+        <div className="login__container">
+          <img src="./login-logo.png" alt="logo" />
+          <div className="login__text">
+            <h1>Sign in Kasel-Chat-App</h1>
+          </div>
+          <Button className="button" onClick={login}>
+            Sign in With Google
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
