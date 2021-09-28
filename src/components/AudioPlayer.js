9e48892b2fc80b412b31458ea3d6b2c54c29a314
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-28 20:01:09 +0530

    Done

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,57 +1,165 @@
 import { CircularProgress } from "@material-ui/core";
-import AudioPlayer from "./AudioPlayer";
+import { PauseRounded, PlayArrowRounded } from "@material-ui/icons";
+import React from "react";
+import "./AudioPlayer.css";
 
-export default function ChatMessages({
-  messages,
-  user,
-  roomId,
-  audioId,
+export default function AudioPlayer({
+  sender,
+  audioUrl,
+  id,
   setAudioId,
+  audioId,
 }) {
-  if (!messages) return null;
+  const [isPlaying, setPlaying] = React.useState(false);
+  const [isMediaLoaded, setMediaLoaded] = React.useState(false);
+  const [isLoaded, setLoaded] = React.useState(false);
+  const [isMetadataLoaded, setMetadataLoaded] = React.useState(false);
+  const [sliderValue, setSliderValue] = React.useState(0);
+  const [duration, setDuration] = React.useState("");
 
-  return messages.map((message) => {
-    const isSender = message.uid === user.uid;
+  const totalDuration = React.useRef("");
+  const audio = React.useRef(new Audio(audioUrl));
+  const interval = React.useRef();
+  const isUploading = React.useRef(audioUrl === "uploading");
 
-    return (
-      <div
-        key={message.id}
-        className={`chat__message ${isSender ? "chat__message--sender" : ""}`}
-      >
-        <span className="chat__name">{message.name}</span>
+  React.useEffect(() => {
+    if (isUploading.current && audioUrl !== "uploading") {
+      audio.current = new Audio(audioUrl);
+      audio.current.load();
+      setLoaded(true);
+    } else if (isUploading.current === false) {
+      setLoaded(true);
+    }
+  }, [audioUrl]);
 
-        {message.imageUrl === "uploading" ? (
-          <div className="image-container">
-            <div className="image__container--loader">
-              <CircularProgress
-                style={{
-                  width: 40,
-                  height: 40,
-                }}
-              />
-            </div>
-          </div>
-        ) : message.imageUrl ? (
-          <div className="image-container">
-            <img src={message.imageUrl} alt={message.name} />
-          </div>
+  function getAudioDuration(media) {
+    return new Promise((resolve) => {
+      media.onloadedmetadata = () => {
+        media.currentTime = Number.MAX_SAFE_INTEGER;
+
+        media.ontimeupdate = () => {
+          media.ontimeupdate = () => {};
+          media.currentTime = 0.1;
+          resolve(media.duration);
+        };
+      };
+    });
+  }
+
+  React.useEffect(() => {
+    if (isLoaded) {
+      getAudioDuration(audio.current).then(() => {
+        setMetadataLoaded(true);
+      });
+    }
+  }, [isLoaded]);
+
+  React.useEffect(() => {
+    if (isMetadataLoaded) {
+      audio.current.addEventListener("canplaythrough", () => {
+        if (!totalDuration.current) {
+          setMediaLoaded(true);
+          const time = formatTime(audio.current.duration);
+          totalDuration.current = time;
+          setDuration(totalDuration.current);
+        }
+      });
+
+      audio.current.addEventListener("ended", () => {
+        clearInterval(interval.current);
+        setDuration(totalDuration.current);
+        setSliderValue(0);
+        setPlaying(false);
+      });
+    }
+  }, [isMetadataLoaded]);
+
+  function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`;
+  }
+
+  function playAudio() {
+    setPlaying(true);
+    audio.current.play();
+    if (audioId !== id) {
+      setAudioId(id);
+    }
+    interval.current = setInterval(updateSlider, 100);
+  }
+
+  function updateSlider() {
+    let sliderPosition = 0;
+
+    const { currentTime, duration } = audio.current;
+    if (typeof duration === "number") {
+      sliderPosition = currentTime * (100 / duration);
+      setSliderValue(sliderPosition);
+      const time = formatTime(currentTime);
+      setDuration(time);
+    }
+  }
+
+  function stopAudio() {
+    audio.current.pause();
+    clearInterval(interval.current);
+    setPlaying(false);
+    setDuration(totalDuration.current);
+  }
+
+  function scrubAudio(event) {
+    const value = event.target.value;
+    const { duration } = audio.current;
+
+    if (isMediaLoaded) {
+      const seekTo = duration * (value / 100);
+      audio.current.currentTime = seekTo;
+      setSliderValue(value);
+    }
+  }
+
+  React.useEffect(() => {
+    if (audioId !== id) {
+      audio.current.pause();
+      setPlaying(false);
+    }
+  }, [audioId, id]);
+
+  return (
+    <>
+      <div className={`audioplayer ${sender ? "" : "audioplayer__alt"}`}>
+        {!isMediaLoaded ? (
+          <CircularProgress />
+        ) : isPlaying ? (
+          <PauseRounded onClick={stopAudio} className="pause" />
+        ) : !isPlaying ? (
+          <PlayArrowRounded onClick={playAudio} />
         ) : null}
-
-        {message.audioName ? (
-          <AudioPlayer
-            sender={isSender}
-            roomId={roomId}
-            id={message.id}
-            audioUrl={message.audioUrl}
-            audioId={audioId}
-            setAudioId={setAudioId}
+        <div>
+          <span
+            style={{ width: `${sliderValue}%` }}
+            className="audioplayer__slider--played"
           />
-        ) : (
-          <span className="chat__message--message">{message.message}</span>
-        )}
-
-        <span className="chat__timestamp">{message.time}</span>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={sliderValue}
+            onChange={scrubAudio}
+            className="audioplayer__slider"
+          />
+        </div>
       </div>
-    );
-  });
+      <span className="chat__timestamp audioplayer__time">{duration}</span>
+    </>
+  );
 }
